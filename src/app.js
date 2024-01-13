6459f2a24fa3f844219503f1dbb6ab700b8e2c49
rohitjingar <rohitjingar264@gmail.com>
2024-01-13 13:19:07 +0530

    utils template

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express"
+import cors from "cors"
+// for the CRUD opertions on the user cookies from our server
+import cookieParser from "cookie-parser";
+const app =  express();
+// this is for the request from our frontend for the data request from this backend 
+app.use(cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials:true
+}))
+// for the json data
+app.use(express.json({
+     limit: "16kb"
+}))
+// for the url data
+app.use(express.urlencoded({
+    extended:true,
+    limit:"16kb"
+}))
+// for the static data like images to store in public
+app.use(express.static("public"))
+app.use(cookieParser())
+export default app;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import dotenv from "dotenv"
 dotenv.config({
     path: './env'
 })
-connectDB();
+connectDB()
+.then(()=>{
+     app.listen(process.env.PORT || 8000, ()=>{
+            console.log(` Server is running at port:  ${process.env.PORT}`);
+     })
+})
+.catch((error)=>{
+        console.log("MONGo db connection failed !!! ", error);
+});
 
 
 
