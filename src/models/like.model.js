f7d089911e3b7514e3e7796de4f54357b5fae779
rohitjingar <rohitjingar264@gmail.com>
2024-02-04 12:29:18 +0530

    like, comment, playlist, tweet model created successfully

---
+++ src/models/like.model.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const likeSchema = new mongoose.Schema({
+    video:{
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+     },
+     likedBy:{
+      type: Schema.Types.ObjectId,
+      ref: "User"
+     },
+     comment:{
+        type: Schema.Types.ObjectId,
+        ref: "Comment"
+     },
+     tweet:{
+        type: Schema.Types.ObjectId,
+        ref: "Tweet"
+     }
+     
+},{timestamps:true})
+
+export const Like = mongoose.model("Like", likeSchema)
