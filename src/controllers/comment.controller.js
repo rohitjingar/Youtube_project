1c801c76b52350745ed7efb79a0c9b4952a0be9a
rohitjingar <rohitjingar264@gmail.com>
2024-02-06 19:18:30 +0530

    comment controller updated successfully

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -9,7 +9,40 @@ const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
-
+    if(!videoId){
+        throw new ApiError(400,"videoId is required")
+    }
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400, "Invalid VideoId")
+    }
+    const video = await Video.findById(videoId)
+    if(!video){
+        throw new ApiError(400, "video does not found")
+    }
+    const videoComments = await Comment.aggregate([
+        {
+          $match:{
+            video: new mongoose.Types.ObjectId(videoId)
+          }
+        },
+        {
+          $sort:{
+             createdAt: -1
+          }
+        }
+    ])
+    const options = {
+        page: parseInt(page),
+        limit: parseInt(limit)
+    }
+    const requiredVideoComments = await Comment.aggregatePaginate(
+        videoComments,
+        options
+    )
+    if(requiredVideoComments.totalDocs===0){
+        return res.status(200).json(new ApiResponse(200, {}, "No Comment found"))
+    }
+    return res.status(200).json(new ApiResponse(200, requiredVideoComments, "Video Comments fetched successfully"))
 })
 
 const addComment = asyncHandler(async (req, res) => {
