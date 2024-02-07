fb81856f26bfbcaa7c37eb9148e5337164fa3447
rohitjingar <rohitjingar264@gmail.com>
2024-02-07 13:23:51 +0530

    createPlaylist updated successfully

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -7,8 +7,19 @@ import asyncHandler from "../utils/asyncHandler.js"
 
 const createPlaylist = asyncHandler(async (req, res) => {
     const {name, description} = req.body
-
     //TODO: create playlist
+    if(!name || !description){
+        throw new ApiError(400, "name and description is required")
+    }
+    const createdPlaylist = await Playlist.create({
+        name,
+        description,
+        owner: req.user?._id
+    })
+    if(!createdPlaylist){
+        throw new ApiError(500, "Something went wrong while creating the playlist")
+    }
+    return res.status(200).json(new ApiResponse(200, createPlaylist, "Playlist created successfully"))
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
